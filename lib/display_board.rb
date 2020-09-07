af6a1bd09f879ec272cf75193e4fcae4ed6df43a
peaceful-prototype-4702 <davidamedina619@gmail.com>
2020-09-07 23:00:38 +0000

    Automatically backed up by Learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -5,8 +5,13 @@ board = [" ", " ", " ", " ", " ", " ", " ", " ", " "]
 
 def display_board(board)
   puts "#{board[0]} | #{board[1]} | #{board[2]}"
-  puts 
-
+  puts "-----------"
+  puts "#{board[3]} | #{board[4]} | #{board[5]}"
+  puts "-----------"
+  puts "#{board[6]} | #{board[7]} | #{board[8]}"
+  
+  
+  display_board(board)
 
 
 
